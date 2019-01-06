57690c6cb47cb14b2e68c7d5c8264cd445b106dd
sindorei <wupan1030@foxmail.com>
2019-01-06 17:13:29 +0800

    simple express

--- express/index.js
+++ express/index.js
@@ -5,15 +5,26 @@ function createApplication() {
         let url = URL.parse(req.url)
         let pathname = url.pathname
         let reqMethod = req.method
-        for(let route of app.routes) {
-            if (route.method === reqMethod && route.path === pathname) {
-                return route.handler(req, res)
+        let index = 0
+        function next(err) {
+           
+            let middleware = app.middlewares[index++]
+            
+            if (middleware) {
+                if (pathname.startsWith(middleware.path)) {
+                    middleware.handler(req, res, next)
+                } 
+            } else {
+                for(let route of app.routes) {
+                    if (route.method === reqMethod && route.path === pathname) {
+                        return route.handler(req, res)
+                    }
+                }
+                res.statusCode = 404
+                res.end(`can not found ${reqMethod} ${pathname}`)
             }
         }
-
-        res.statusCode = 404
-        res.end(`can not found ${reqMethod} ${pathname}`)
-
+        next()
     }
     app.listen = function () {
         const server = http.createServer(app)
@@ -21,18 +32,31 @@ function createApplication() {
     }
 
     app.routes = []
+    app.middlewares = []
 
     http.METHODS.forEach(method => {
         let lowerCaseMethod = method.toLowerCase()
         app[lowerCaseMethod] = function (path, handler) {
-            let layser = {
+            let layer = {
                 method,
                 path,
                 handler
             }
-            app.routes.push(layser)
+            app.routes.push(layer)
         }
     })
+
+    app.use = function(path, handler) {
+        if (typeof handler !== 'function') {
+            handler = path
+            path = '/'
+        }
+        let layer = {
+            path,
+            handler
+        }
+        app.middlewares.push(layer)
+    }
 
     return app
 }
--- server_demo/express.js
+++ server_demo/express.js
@@ -2,6 +2,17 @@ const express = require('../express')
 
 const app = express()
 
+
+app.use(function(req, res, next) {
+    console.log('middleware 1')
+    next()
+})
+
+app.use('/hello', function(req, res, next) {
+    console.log('hello middleware...')
+    next()
+})
+
 app.get('/hello', (req, res) => {
     res.end('Hello!')
 })
